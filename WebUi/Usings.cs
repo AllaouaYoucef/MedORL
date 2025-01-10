2011dbc901211ef4a4b3a059a2d07823bcf007c1
allaoua <allaouayoucef1@gmail.com>
2025-01-10 09:48:54 +0100

    updated global using

--- WebUi/Usings.cs
+++ WebUi/Usings.cs
@@ -3,3 +3,6 @@ global using BLL;
 global using WebUi;
 global using BLL.Interfaces;
 global using BLL.Managers;
+global using Microsoft.AspNetCore.Mvc;
+global using WebUi.Models;
+global using BLL.DTOs;
